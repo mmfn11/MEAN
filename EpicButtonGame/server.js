3e2c6b73a8290c802e2048f0d3f93a374709de4f
mmfn11 <mmfn11@gmail.com>
2019-10-05 16:01:06 +0300

    Assignment:2

--- EpicButtonGame/server.js
+++ EpicButtonGame/server.js
@@ -13,17 +13,24 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 var server = app.listen(8000)
-var io = require('socket.io').listen(server);
-io.sockets.on('connection', function (socket) {
-    
-    socket.on("posting_form", function (data){
+var count=0;
 
-    var random_number = Math.floor((Math.random() * 1000) + 1);
-    socket.emit('updated_message', {response: data}); 
-    socket.emit('random_number', {response: random_number}); 
-   });
+app.get('/', (req, res) => {
+    res.render('index', { count });
 });
 
-app.get('/', function(req, res) {
-    res.render("index");
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    
+
+    socket.on('count_clicked', function (data) {
+        count++;
+        io.emit('count_response', { response: count });
+    });
+
+    socket.on('reset_clicked', function (data){
+        count = 0;
+        io.emit('reset_response', { response: count });
+    });
 });
